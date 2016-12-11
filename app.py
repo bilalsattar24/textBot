50d59e58bdc5ef6e6cb78deb95526e9a4f66b520
ryanlb22 <rlebon@csumb.edu>
2016-12-10 20:10:24 -0800

    added response

--- app.py
+++ app.py
@@ -12,7 +12,7 @@ def sms():
     message_body = request.form['Body']
 
     #you can change this
-    menu = '1. BIT\n2. Library\n3. Dining Commons\n4. Student Center\n5. Gym'
+    menu = 'Welcome to CSUMB textBot Menu\n 1. BIT\n2. Library\n3. Dining Commons\n4. Student Center\n5. Gym \n 6. Dr. Tao'
     resp = twiml.Response()
     option = message_body
     #dont touch
@@ -23,15 +23,17 @@ def sms():
     ############################
     if option=='1':
         #msg.media("")
-        sent="Bit builiding is open from 8AM to 8PM\n"
+        sent="Name: Joel & Dena Gambord Business & Information Technology Building \n Address: 3052 Divarty St, Marina CA 93933 \n Hours: 8 AM to 10 PM \n Fact: Did you know the official naming took place on February 25, 2016. \n "
     elif option=='2':
-        sent="Library is open from 8AM to 10PM"
+        sent="Name: Tanimura & Antle Family Memorial Library \n Address: 3054 Divarty St, Seaside, CA 93955 \n Hours: 8 AM to 12 AM \n Fact: Library closes at 4 in the morning during finals. \n "
     elif option=='3':
-        sent=""
+        sent="Name: Dining Commons \n Address: 3112 Inter-Garrison Rd, Seaside, CA 93955 \n Hours: 7 AM to 2 PM, 5 PM to 8 PM \n Fact: From 11 AM-12 PM lunch is $4. \n "
     elif option=='4':
-        sent=""
+        sent="Name: Student Center \n Address: Alumni & Visitors Center, 5108 Fourth Ave, Marina, CA 93933 \n Hours: 8 AM to 5 PM \n Fact: You can rent out equipment with a drivers license. \n "
     elif option=='5':
-        sent=""
+        sent="Name: Otter Sports Center \n Address: 2050 Inter-Garrison Rd, Marina, CA 93933 \n Hours: 6 AM to 12 Midnight \n Fact: The gym has recieved several different upgrades. \n "
+    elif option=='6':
+        sent="Dr Tao \n Education: Ph.D. in Computer Engineering, University of California at Irvine.\n The best professor in Computer Architecture"
     else:
         sent=menu
     #response
