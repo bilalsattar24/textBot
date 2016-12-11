ec461d0505ff1704bb225c45bb227f9977c5e855
bilalsattar24 <bilalsattar24@gmail.com>
2016-12-10 19:31:37 -0800

    reponses working, needs content

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 from flask import Flask, request
 from twilio import twiml
+import sys
  
  
 app = Flask(__name__)
@@ -9,9 +10,22 @@ app = Flask(__name__)
 def sms():
     number = request.form['From']
     message_body = request.form['Body']
- 
+
+    #you can change this
+    menu = '1. BIT\n2. Library\n3. Dining Commons\n4. Gym'
     resp = twiml.Response()
-    resp.message('Hello {}, you said: {}'.format(number, message_body))
+    
+    #dont touch
+    if message_body == 'exit':
+        sys.exit()
+    
+    #resp.message('Hello {}, you said: {}'.format(number, message_body))
+    ############################
+
+    #response
+    resp.message(menu)
+    
+    
     return str(resp)
  
 if __name__ == '__main__':
