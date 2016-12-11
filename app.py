d3af242c238099bd57326334018e7ce4b2ccc64e
Daniel Jauregui <djauregui@csumb.edu>
2016-12-10 19:38:55 -0800

    Updated options menu

--- app.py
+++ app.py
@@ -12,18 +12,30 @@ def sms():
     message_body = request.form['Body']
 
     #you can change this
-    menu = '1. BIT\n2. Library\n3. Dining Commons\n4. Gym'
+    menu = '1. BIT\n2. Library\n3. Dining Commons\n4. Student Center\n5. Gym'
     resp = twiml.Response()
-    
+    option = message_body
     #dont touch
     if message_body == 'exit':
         sys.exit()
     
     #resp.message('Hello {}, you said: {}'.format(number, message_body))
     ############################
-
+    if option=='1':
+        sent="Bit builiding is open from 8AM to 8PM\n"
+    elif option=='2':
+        sent="Library is open from 8AM to 10PM"
+    elif option=='3':
+        sent=""
+    elif option=='4':
+        sent=""
+    elif option=='5':
+        sent=""
+    else:
+        sent=menu
     #response
     resp.message(menu)
+
     
     
     return str(resp)
